67f6e6abbe0443c9a0aad6ea774b4079a76fb349
Max Walts <mwalts@umich.edu>
2020-04-05 14:31:00 -0500

    Initial Frontend commit

---
+++ src/components/homepage/AboutTeam.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function AboutTeam() {
+    return (
+        <div class="card">
+            <div class="card-body">
+                <h3>About Us</h3>
+                <p>Our names, contact</p>
+            </div>
+        </div>
+    )
+}
+
+export default AboutTeam
---
+++ src/components/homepage/UXinfo.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function UXinfo() {
+    return (
+        <div class="card">
+            <div class="card-body">
+                <h3>Info for UX professionals</h3>
+                <p>these are the features. these are the features. these are the features.</p>
+            </div>
+        </div>
+    )
+}
+
+export default UXinfo
