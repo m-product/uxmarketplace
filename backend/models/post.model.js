52d772823a1edb0c7c375a27e4e487f21a95995f
Max Walts <mwalts@umich.edu>
2020-04-05 19:26:29 -0500

    Setup temporary endpoints (#1)
    Co-authored-by: bwal <bwal@umich.edu>

---
+++ backend/models/post.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema({
+  description: {
+    type: String,
+    required: true
+  },
+  username: {
+      type: String, 
+      required: true,
+      trim: true
+  }
+});
+
+const User = mongoose.model('Post', postSchema);
+
+module.exports = Post;
