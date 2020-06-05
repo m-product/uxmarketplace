600824d2f86bfa042bdaceb929371f3130a1d858
sszaman <sszaman@umich.edu>
2020-06-05 16:16:06 -0400

    format of likes and comments section

--- front-end/src/components/projectResponses/ProjectResponse.js
+++ front-end/src/components/projectResponses/ProjectResponse.js
@@ -9,9 +9,10 @@ const ProjectResponse = ({username, likes, comments}) => {
         <div className="card bg-light m-2" style={{width: '18rem'}}>
             <img className="card-img-top" src={require('../../assets/placeholder.png')} alt="Card image cap"/>
             <div className="card-body">
-                <h5 className="card-title">{username}</h5>
-                <p className="card-text">{likes}</p>
-                <p className="card-text">{comments}</p>
+                <div className="row">
+                    <p className="card-title text-left col-sm-6">{username}</p>
+                    <p className="card-text text-right col-sm-6">{likes}  {comments}</p>
+                </div>
                 <a href="#" className="btn btn-primary">Submit Feedback</a>
             </div>`
         </div>
