ff4ab919a7487655a13a391dd4e51635b5a7a646
bwal <bwal@umich.edu>
2020-06-06 20:38:49 -0400

    Minor adjusments to styling / button layout

--- front-end/src/components/CommentsPage/Comment.js
+++ front-end/src/components/CommentsPage/Comment.js
@@ -1,39 +1,31 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Collapse from 'react-bootstrap/Collapse'
-import { Link } from 'react-router-dom';
 
-const Comment = ({user, content, heading}) => {
+const Comment = ({ user, content, heading }) => {
     const [open, setOpen] = useState(false);
     return (
         <>
-        <div className='container w-50'
-            onClick={() => setOpen(!open)}
-            aria-controls="example-collapse-text"
-            aria-expanded={open}
-        >
-            <div className='row rounded-lg bg-light mt-3'>
-                <div className="col-auto p-4">
-                    <h3>{user}</h3>
-                    <p className='text-muted'>{heading}</p>
-                </div>
-            </div>
-        </div> 
-        <Collapse in={open}>
-            <div className='container w-50 bg-light'>
-                <div  className='row'>
-                    <div className='col p-4'>
-                        <p className='text-muted' >{content}</p>
-                    </div>
-                </div>
-                <div className='row'>
-                    <div className='col-auto p-4'>
-                        <Link to='/project-response-list'>
-                        <button className='btn btn-outline-primary purple'>Back to Responses</button>
-                        </Link>
+            <div className='container w-50'
+                onClick={() => setOpen(!open)}
+                aria-controls="example-collapse-text"
+                aria-expanded={open}
+            >
+                <div className='row rounded-lg bg-light mt-3'>
+                    <div className="col-auto p-3">
+                        <h3>{user}</h3>
+                        <p className='text-muted'>{heading}</p>
                     </div>
                 </div>
             </div>
-        </Collapse>
+            <Collapse in={open}>
+                <div className='container w-50 bg-light'>
+                    <div className='row'>
+                        <div className='col'>
+                            <p className='text-muted' >{content}</p>
+                        </div>
+                    </div>
+                </div>
+            </Collapse>
         </>
     );
 };
--- front-end/src/components/CommentsPage/CommentList.js
+++ front-end/src/components/CommentsPage/CommentList.js
@@ -1,20 +1,27 @@
 import React, {useState, useContext} from 'react';
 import Comment from './Comment';
 import { CommentContext } from './CommentContext';
+import { Link } from 'react-router-dom';
 
 const CommentList = () => {
     const [comments, setComments] = useContext(CommentContext);
 
     return (
         <div>
-        <div className='container p-5'>
+        <div className='container p-4'>
                 <div className='row justify-content-center'>
-                    <h1 className='text-body'>Comments</h1>
+                    <h1 className='text-body'>Feedback</h1>
                 </div>
                 <div className='row justify-content-center p-2 '>
                     <h6 className='text-muted'>Discuss your thoughts on this project!</h6>
                 </div>
+                <div className='row justify-content-center p-2'>
+                        <Link to='/project-response-list'>
+                        <button className='btn btn-md btn-outline-dark'>Back to Project Responses</button>
+                        </Link>
+                </div>
         </div>
+
         <div className='container'>
             {comments.map(comment => (
             <Comment user={comment.user} content={comment.content} heading={comment.heading} key={comment.id}/>
